80100541afe3e6785b6b616aef1117bbd6fd7193
hugoresende27 <Hugoresende27@gmail.com>
2021-09-26 15:31:29 +0100

    dom

---
+++ core/classes/Database.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace core\classes;
+
+
+class Database{
+
+    /*gestão do banco de dados*/
+    public function __construct()
+    {
+        echo "Base de dados!";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,4 +1,30 @@
 <?php
 
 // abrir a sessão 
+
+use core\classes\Database;
+use core\classes\Functions;
+
 session_start();
+
+// carregar o config
+require_once('../config.php');
+
+// carrega todas as classes do projeto
+require_once('../vendor/autoload.php');
+
+$a=new Database();
+$b=new Functions();
+$b->teste();
+
+
+echo "OK";
+
+/*
+carregar o config
+carregar classes
+carregar sistema de rotas:
+    -mostrar loja
+    -mostrar carrinho
+    -mostrar backoffice
+*/
