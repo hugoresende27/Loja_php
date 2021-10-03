09c26b39b23b913cca557042d0819733da6d1179
hugoresende27 <Hugoresende27@gmail.com>
2021-10-03 12:08:26 +0100

    dom

--- core/views/layouts/html_header.php
+++ core/views/layouts/html_header.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+
     <title>Document</title>
 </head>
 <body>
